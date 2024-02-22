fc27646a3cd37137554ada746f5df6173500936a
aartikas <aartika15082@iiitd.ac.in>
2024-02-22 22:32:26 +0530

    Time: 26 ms (71.76%) | Memory: 52.8 MB (53.29%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        Arrays.sort(points, new XYZ());
+        int[][] ans = new int[k][2];
+
+        for(int i = 0 ; i <k ;i++){
+            ans[i][0] = points[i][0];
+            ans[i][1] = points[i][1];
+
+        }
+
+        return ans;
+        
+    }
+}
+
+class XYZ implements Comparator<int[]>{
+    @Override
+    public int compare(int[] a , int[] b){
+        int x1 = a[0];
+        int y1 = a[1];
+        int d1 = (x1*x1)+(y1*y1);
+        int x2 = b[0];
+        int y2 = b[1];
+        int d2 = (x2*x2)+(y2*y2);
+
+        return d1-d2;
+    } 
+
+}
