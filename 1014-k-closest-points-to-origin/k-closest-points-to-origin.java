14b34abdfee94a9405d7626a611fc0eb4e600e95
aartikas <aartika15082@iiitd.ac.in>
2024-02-22 22:32:57 +0530

    Time: 25 ms (74.31%) | Memory: 52.3 MB (60.03%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -4,8 +4,7 @@ class Solution {
         int[][] ans = new int[k][2];
 
         for(int i = 0 ; i <k ;i++){
-            ans[i][0] = points[i][0];
-            ans[i][1] = points[i][1];
+            ans[i] = points[i];
 
         }
 
