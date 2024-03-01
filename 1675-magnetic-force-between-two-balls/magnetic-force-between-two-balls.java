d12db44352099ceba64f64e4726852a50de6b8aa
aartikas <aartika15082@iiitd.ac.in>
2024-03-01 19:52:07 +0530

    Time: 42 ms (80.97%) | Memory: 55.8 MB (63.25%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,33 +1,34 @@
 class Solution {
     public int maxDistance(int[] position, int m) {
         Arrays.sort(position);
-        int lo = 0;
-        int hi = position[position.length-1];
-        int optimal = 0;
-        while (lo <= hi) {
-            int mid = lo + (hi-lo)/2;
-            if (canPut(position, m, mid)) {
-                optimal = mid;
-                lo = mid+1;
-            } else {
-                hi = mid-1;
+        int N = position.length;
+        int l = 0;
+        int h = position[N-1];
+        int ans = 0;
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(ispossible(position,m,mid)){
+                ans = mid;
+                l = mid+1;
+            }else{
+                h = mid-1;
             }
+
         }
-        return optimal;
+        return ans;
+        
     }
 
-    /*
-    * returns whether we can put m balls such that minimum distance between two consecutive ball is always greater than or equal to the max.
-    */
-    private boolean canPut(int[] positions, int m, int max) {
-        int count = 1;
-        int last = positions[0];
-        for (int i = 0; i < positions.length; i++) {
-            if (positions[i] - last >= max) {
-                last = positions[i];
-                count++;
+    public boolean ispossible(int[] position,int m,int mid){
+        int cnt=1;
+        int lst = position[0];
+        for(int i =0;i<position.length;i++){
+            if(position[i]-lst>=mid){
+                lst = position[i];
+                cnt++;
             }
         }
-        return count >= m;
+
+        return cnt>=m;
     }
 }
